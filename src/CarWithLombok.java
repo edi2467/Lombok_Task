7ff26adf61f96646bc08cf415ce0d0d5fd9a05bf
edi2467 <edirinnuhu123@gmail.com>
2026-03-30 20:19:59 +0200

    update

--- src/CarWithLombok.java
+++ src/CarWithLombok.java
@@ -21,14 +21,16 @@ class CarDemoWithLombok {
     public static void main(String[] args) {
 
         CarWithLombok CarWithLombok1 = new CarWithLombok("GLE", "Mercedes", 2020, 350000, "white", 5);
-        CarWithLombok CarWithLombok2 = new CarWithLombok("GLC", "Mercedes", 2023, 200000, "silver", 3);
+        CarWithLombok CarWithLombok2 = new CarWithLombok("GLE", "Mercedes", 2020, 350000, "white", 5);
         System.out.println(CarWithLombok1);
         System.out.println(CarWithLombok2);
         System.out.println(CarWithLombok1.getColor());
         int year = CarWithLombok1.getYear();
-        CarWithLombok1.setColor("black");
+
         System.out.println("Are these objects equal? " + CarWithLombok1.equals(CarWithLombok2));
         System.out.println("The HashCode of this object is: " + CarWithLombok2.hashCode());
         System.out.println("The HashCode of this object is: " + CarWithLombok1.hashCode());
+
+        CarWithLombok1.setColor("black");
     }
 }
