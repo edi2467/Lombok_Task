e14d834387496cb6ebb85f1b7aabfcff47cea35b
edi2467 <edirinnuhu123@gmail.com>
2026-03-31 14:29:50 +0200

    update

--- src/CarWithLombok.java
+++ src/CarWithLombok.java
@@ -1,12 +1,12 @@
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.ToString;
+
 
 @Data
 @AllArgsConstructor
-@EqualsAndHashCode
-@ToString
+@EqualsAndHashCode(of ="brand")
+
 
 public class CarWithLombok {
     private String model;
@@ -21,16 +21,16 @@ class CarDemoWithLombok {
     public static void main(String[] args) {
 
         CarWithLombok CarWithLombok1 = new CarWithLombok("GLE", "Mercedes", 2020, 350000, "white", 5);
-        CarWithLombok CarWithLombok2 = new CarWithLombok("GLE", "Mercedes", 2020, 350000, "white", 5);
+        CarWithLombok CarWithLombok2 = new CarWithLombok("GLC", "Mercedes", 2023, 200000, "silver", 3);
         System.out.println(CarWithLombok1);
         System.out.println(CarWithLombok2);
         System.out.println(CarWithLombok1.getColor());
         int year = CarWithLombok1.getYear();
-
+        CarWithLombok1.setColor("black");
         System.out.println("Are these objects equal? " + CarWithLombok1.equals(CarWithLombok2));
         System.out.println("The HashCode of this object is: " + CarWithLombok2.hashCode());
         System.out.println("The HashCode of this object is: " + CarWithLombok1.hashCode());
 
-        CarWithLombok1.setColor("black");
+
     }
 }
